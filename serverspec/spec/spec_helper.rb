3bc920055cd764b5f5d38ed8dfcdfbe82afed738
tushiko23 <148033059+tushiko23@users.noreply.github.com>
2024-12-16 22:07:57 +0900

    helper.rbを修正2

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -3,24 +3,26 @@ require 'net/ssh'
 
 set :backend, :ssh
 
-if ENV['ASK_SUDO_PASSWORD']
-  begin
-    require 'highline/import'
-  rescue LoadError
-    fail "highline is not available. Try installing it."
-  end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-else
-  set :sudo_password, ENV['SUDO_PASSWORD']
-end
+#if ENV['ASK_SUDO_PASSWORD']
+#  begin
+#    require 'highline/import'
+#  rescue LoadError
+#    fail "highline is not available. Try installing it."
+#  end
+#  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
+#else
+#  set :sudo_password, ENV['SUDO_PASSWORD']
+#end
 
-host = ENV['TARGET_HOST'] || 'target' # ~/.ssh/config からHost名を参照
 
-options = Net::SSH::Config.for(host) 
+options = Net::SSH::Config.for('target') # ~/.ssh/config からHost名を参照
 
 options[:user] ||= 'ec2-user'
 
-set :host,        options[:host_name] || host
+
+host = 'target' # ~/.ssh/config からHost名を参照
+
+set :host,        
 set :ssh_options, options
 
 # Disable sudo
