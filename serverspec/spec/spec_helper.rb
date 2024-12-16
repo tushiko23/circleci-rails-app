09af09f508aba215b575b64677f9eb194cdff699
tushiko23 <148033059+tushiko23@users.noreply.github.com>
2024-12-17 04:18:09 +0900

    hostのみで動作確認

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -3,12 +3,12 @@ require 'net/ssh'
 
 set :backend, :ssh
 
-host = ENV['TARGET_HOST_NAME']
+host = ENV['TARGET_HOST']
 options = Net::SSH::Config.for(host) 
 options[:keys] = ['~/.ssh/id_rsa']
 options[:user] ||= 'ec2-user'
 
-options[:host_name] = ENV['TARGET_HOST_NAME'] if ENV['TARGET_HOST_NAME']
+options[:host_name] = ENV['TARGET_HOST'] if ENV['TARGET_HOST']
 
 set :host, options[:host_name] || host       
 set :ssh_options, options
